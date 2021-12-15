62fe9fb45004cd34e5f799cb585fce7ddf92e9ef
Eduardo Brod <eduardo_brod@hotmail.com>
2021-12-14 23:25:00 -0300

    v1.0

---
+++ js/script1.js
@@ -0,0 +1,60 @@
+class Produto {
+
+    constructor (){
+        this.id = 1;
+        this.arrayProdutos = [];
+    }
+
+    salvar(){
+        
+        let produto = this.lerDados();
+
+        if(this.validaCampos(produto)){
+            this.adicionar(produto);
+        }
+
+        console.log(this.arrayProdutos);
+    }
+
+    adicionar(produto){
+        this.arrayProdutos.push(produto);
+        this.id++;
+    }
+
+    lerDados(){
+        let produto = {};
+
+        produto.id = this.id;
+        produto.nomeProduto = document.getElementById('produto').value;
+        produto.preco = document.getElementById('preco').value;
+
+        return produto
+    }
+
+    validaCampos(produto){
+
+        let msg = '';
+
+        if(produto.nomeProduto == ''){
+            msg += 'Informe o nome do produto \n';
+        }
+        if(produto.preco == ''){
+            msg += 'Informe o preço do produto \n';
+        }
+
+        if(msg != ''){
+            alert(msg);
+            return false;
+        }
+
+        return true;
+
+    }
+
+    cancelar(){
+        alert("Excluido com sucesso");
+    }
+
+}
+
+var produto = new Produto();
